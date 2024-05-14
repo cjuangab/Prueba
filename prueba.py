e1f61c20d78a73b225ac50c08fb2b9717374deaa
cjuangab <cjuangab@gmail.com>
2024-05-13 22:49:05 -0300

    Prueba de commit

---
+++ prueba.py
@@ -0,0 +1,11 @@
+a = input("Nombre: ")
+i = 0
+if a == "Gabriel" or a=="Florencia":
+    print("Bienvenido ", a)
+    i = int(input("Numerito: "))
+else:
+    a = input("Quien so vo ")
+    i = int(input("Decime un numerito b: "))
+       
+for j in range(0,i):
+    print("Hola", a) 
